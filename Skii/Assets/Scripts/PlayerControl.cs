a3bc5cca5342109b100a5b2ccda68e4135b73a5d
nayttyan <152870652+nayttyan@users.noreply.github.com>
2026-04-13 15:45:16 +0300

    1 lesson done

---
+++ Skii/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private InputAction move;
+    [SerializeField] private float rotSpeed = 30, speed = 20;
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        move = InputSystem.actions.FindAction("Player/Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
+        Vector2 moveInput = move.ReadValue<Vector2>();
+        Debug.Log("x: "+ moveInput.x + " y: " + moveInput.y);
+        transform.Rotate(0, -moveInput.x * rotSpeed * Time.fixedDeltaTime, 0);
+        float turnAngle = Mathf.Abs(180 - transform.localEulerAngles.y);
+        float speedMult = Mathf.Cos(turnAngle * Mathf.Deg2Rad);
+        Debug.Log("turn angle: " + turnAngle);
+    }
+}
